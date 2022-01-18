c2a8d945e5ec66eb58dd5520d2e43a0beccb8d90
Nikita(PC) <nikita.verba04@mail.ru>
2022-01-18 17:40:02 +0200

    Switched from sqlite to postgresql db

--- markov_chain/settings.py
+++ markov_chain/settings.py
@@ -78,8 +78,12 @@ WSGI_APPLICATION = 'markov_chain.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql_psycopg2',
+        'NAME': os.environ.get('DB_NAME'),
+        'USER': os.environ.get('DB_USER'),
+        'PASSWORD': os.environ.get('DB_PASSWORD'),
+        'HOST': 'localhost',
+        'PORT': ''
     }
 }
 
